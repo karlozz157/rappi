bd807e8801b25d1d0f7b642abc7f2bf12de97221
karlozz157 <karlozz157@gmail.com>
2017-01-29 14:18:31 -0600

    Added form to process input

---
+++ src/RappiBundle/Form/Type/CubeSummationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RappiBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CubeSummationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('input', 'textarea', [
+                'required' => true,
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+        ));
+    }
+}
